ac4525270bcbae869ad6f5b1165fab9043d8c14e
Disego Maile <disegonoah@gmail.com>
2026-03-12 16:14:25 +0200

    Remove venv from repository

--- venv/Lib/site-packages/prefect/server/ui-v2/assets/index-D_Wxvt_x.js
+++
@@ -1,2 +0,0 @@
-const o=function(){return null};export{o as component};
-//# sourceMappingURL=index-D_Wxvt_x.js.map
--- venv/Lib/site-packages/prefect/server/ui/assets/useCan-zdb3lP4V.js
+++
@@ -1,2 +0,0 @@
-import{cS as n}from"./index-CWv_Pcr2.js";const c=Symbol("canInjectionKey");function a(){return n(c)}export{c,a as u};
-//# sourceMappingURL=useCan-zdb3lP4V.js.map
